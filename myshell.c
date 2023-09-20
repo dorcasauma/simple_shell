c5d8595b8fa22f219c29ac2d7369699cf2e27a23
DORCAS AUMA <dorcasauma89@gmail.com>
2023-09-20 13:01:08 +0300

    my_shell

--- myshell.c
+++ myshell.c
@@ -1,33 +1,55 @@
-import os
-import sys
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-def prompt():
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+int main() {
+    char *userInput = NULL;
+    size_t userInputSize = 0;
+    char *shellCommand = "/bin/bash";
+    char *envp[] = {NULL};
+    int execveResult;
+    int status;
+    pid_t my_pid;
 
-def main():
-    while True:
-        prompt()
-        try:
-            command = input()
-            if not command:
-                continue
-        except EOFError:
-            print()  # Print a newline when Ctrl+D is pressed
-            break
-        
-        pid = os.fork()
-        
-        if pid == 0:  # Child process
-            try:
-                os.execve(command, [command], os.environ)
-            except FileNotFoundError:
-                print(f"Command not found: {command}")
-            except Exception as e:
-                print(f"An error occurred: {e}")
-            sys.exit()
-        else:
-            os.waitpid(pid, 0)  # Wait for the child process to finish
+    while (1) {
+        printf("mysimpleshell $: ");
+        ssize_t bytesRead = getline(&userInput, &userInputSize, stdin);
 
-if __name__ == "__main__":
-    main()
+        if (bytesRead == -1) {
+            perror("getline");
+            exit(EXIT_FAILURE);
+        }
+
+        userInput[bytesRead - 1] = '\0';
+
+        if (strcmp(userInput, "exit") == 0) {
+            free(userInput);
+            exit(0);
+        }
+
+        char *shellArgs[] = {shellCommand, "-c", userInput, NULL};
+
+        my_pid = fork();
+
+        if (my_pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (my_pid == 0) {
+            execveResult = execve(shellCommand, shellArgs, envp);
+            perror("execve");
+            free(userInput);  // Free allocated memory before exiting child process
+            exit(EXIT_FAILURE);
+        } else {
+            waitpid(my_pid, &status, 0);
+            printf("You entered: %s\n", userInput);
+        }
+    }
+
+    free(userInput);  // Free allocated memory before exiting the main process
+    return 0;
+}
