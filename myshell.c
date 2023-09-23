813a4b5781d5a7dd1494c3f79af00e83ea58fee2
DORCAS AUMA <dorcasauma89@gmail.com>
2023-09-23 22:48:06 +0300

    separator

--- myshell.c
+++ myshell.c
@@ -2,66 +2,61 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-#define MAX_BUFFER_SIZE 1024
+#define MAX_COMMANDS 10
+#define MAX_COMMAND_LENGTH 100
 
 int main() {
-    char userInput[MAX_BUFFER_SIZE];
-    char currentDir[MAX_BUFFER_SIZE];
-    char previousDir[MAX_BUFFER_SIZE];
+    char input[MAX_COMMAND_LENGTH];
+    char *commands[MAX_COMMANDS];
+    int num_commands = 0;
 
-    while (1) {
-        printf("MyShell> ");
-        if (fgets(userInput, sizeof(userInput), stdin) == NULL) {
-            break;
+    char *token;
+    size_t len;
+    int i;
+
+    printf("Enter commands (separated by ;): ");
+    fgets(input, sizeof(input), stdin);
+
+    token = strtok(input, ";");
+    while (token != NULL && num_commands < MAX_COMMANDS) {
+        commands[num_commands] = strdup(token);
+        num_commands++;
+        token = strtok(NULL, ";");
+    }
+
+    for (i = 0; i < num_commands; i++) {
+        char *command = commands[i];
+        int status;
+
+        while (*command == ' ' || *command == '\t') {
+            command++;
         }
 
+        len = strlen(command);
 
-        userInput[strcspn(userInput, "\n")] = '\0';
-
-        if (getcwd(previousDir, sizeof(previousDir)) == NULL) {
-            perror("getcwd");
-            continue;
+        while (len > 0 && (command[len - 1] == ' ' || command[len - 1] == '\t' || command[len - 1] == '\n')) {
+            command[len - 1] = '\0';
+            len--;
         }
 
-
-        if (strncmp(userInput, "cd", 2) == 0) {
-            char *args = userInput + 2;
-
-            while (*args == ' ' || *args == '\t') {
-                args++;
+        if (strlen(command) > 0) {
+            pid_t pid = fork();
+            if (pid == 0) {
+                execlp("/bin/sh", "/bin/sh", "-c", command, NULL);
+                perror("exec");
+                exit(1);
+            } else if (pid < 0) {
+                perror("fork");
+                exit(1);
+            } else {
+                waitpid(pid, &status, 0);
             }
-            if (*args == '\0') {
-                args = getenv("HOME");
-            } else if (strcmp(args, "-") == 0) {
-
-                args = getenv("OLDPWD");
-                if (args == NULL) {
-                    fprintf(stderr, "OLDPWD environment variable not set\n");
-                    continue;
-                }
-            }
-
-
-            if (chdir(args) != 0) {
-                perror("chdir");
-                continue;
-            }
-
-            if (getcwd(currentDir, sizeof(currentDir)) == NULL) {
-                perror("getcwd");
-                continue;
-            }
-            if (setenv("PWD", currentDir, 1) != 0) {
-                perror("setenv");
-            }
-            if (setenv("OLDPWD", previousDir, 1) != 0) {
-                perror("setenv");
-            }
-        } else {
-
-            printf("Executing command: %s\n", userInput);
         }
+
+        free(commands[i]);
     }
 
     return 0;
