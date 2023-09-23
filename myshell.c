9fa2eb649c435429addaf53aeaf6c33125313790
DORCAS AUMA <dorcasauma89@gmail.com>
2023-09-23 22:19:12 +0300

    setenv

--- myshell.c
+++ myshell.c
@@ -1,282 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <unistd.h>
 
-#define MAX_PATH_LEN 256
+#define MAX_ARGS 64
 
-void executeCommand(char *shellCommand, char **shellArgs, char *envp[]);
-void cleanupAndExit(char *userInput, char **shellArgs);
-void removeLeadingSpaces(char *str);
-void removeTrailingSpaces(char *str);
-void printEnvironment();
-char *findCommandPath(const char *command);
-void printShellArgs(char **shellArgs);
+void cleanupAndExit(char *userInput, int exit_status);
+void executeCommand(char **shellArgs);
+
 ssize_t custom_getline(char **lineptr, size_t *n, FILE *stream);
 
-int main(void)
-{
+int main(void) {
     char *userInput = NULL;
-    char *token;
     size_t userInputSize = 0;
-    char *envp[] = {NULL};
-    int status;
-    pid_t my_pid;
+    int exit_status = 0;
+    char *shellArgs[MAX_ARGS];
+
     ssize_t bytesRead;
-    size_t newline_pos;
 
-    char **shellArgs = malloc(4 * sizeof(char *));
-    if (shellArgs == NULL)
-    {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-
-    while (1)
-    {
-        int argCount = 0;
-
+    while (1) {
+        printf("$ ");
         bytesRead = custom_getline(&userInput, &userInputSize, stdin);
-        if (bytesRead == -1)
-        {
-            cleanupAndExit(userInput, shellArgs);
-        }
-        if (strcmp(userInput, "exit") == 0)
-        {
-            cleanupAndExit(userInput, shellArgs);
-        }
-        removeLeadingSpaces(userInput);
-        removeTrailingSpaces(userInput);
 
-        newline_pos = strcspn(userInput, "\n");
-        userInput[newline_pos] = '\0';
-
-        printf("userninput %s", userInput);
-
-        token = strtok(userInput, " ");
-        while (token != NULL)
-        {
-            shellArgs[argCount++] = token;
-            token = strtok(NULL, " ");
+        if (bytesRead == -1) {
+            perror("getline");
+            exit(1);
         }
 
-        if (strchr(shellArgs[0], '/') == NULL)
-        {
-            char *new_command_path = findCommandPath(shellArgs[0]);
-            if (new_command_path == NULL)
-            {
-                printf("%s: not found\n", shellArgs[0]);
-                continue;
+        if (bytesRead == 0) {
+            break;
+        }
+
+
+        if (shellArgs[0] != NULL) {
+            if (strcmp(shellArgs[0], "exit") == 0) {
+                if (shellArgs[1] != NULL) {
+                    exit_status = atoi(shellArgs[1]);
+                }
+                break;
+            } else if (strcmp(shellArgs[0], "setenv") == 0) {
+                if (shellArgs[1] != NULL && shellArgs[2] != NULL) {
+                    if (setenv(shellArgs[1], shellArgs[2], 1) != 0) {
+                        fprintf(stderr, "setenv: Failed to set environment variable.\n");
+                    }
+                } else {
+                    fprintf(stderr, "setenv: Usage: setenv VARIABLE VALUE\n");
+                }
+            } else if (strcmp(shellArgs[0], "unsetenv") == 0) {
+                if (shellArgs[1] != NULL) {
+                    if (unsetenv(shellArgs[1]) != 0) {
+                        fprintf(stderr, "unsetenv: Failed to unset environment variable.\n");
+                    }
+                } else {
+                    fprintf(stderr, "unsetenv: Usage: unsetenv VARIABLE\n");
+                }
+            } else {
+                executeCommand(shellArgs);
             }
-            strcpy(shellArgs[0], new_command_path);
         }
 
-        if (userInput[0] == '\0')
-        {
-            continue;
-        }
-        if (strcmp(shellArgs[0], "/bin/env") == 0)
-        {
-            printEnvironment();
-            continue;
-        }
-        if (access(shellArgs[0], X_OK) != 0)
-        {
-            fprintf(stderr, "Error: Command '%s' does not exist or is not executable.\n", shellArgs[0]);
-            continue;
-        }
-
-        my_pid = fork();
-        if (my_pid == -1)
-        {
-            perror("fork");
-            cleanupAndExit(userInput, shellArgs);
-        }
-        if (my_pid == 0)
-        {
-            executeCommand(shellArgs[0], shellArgs, envp);
-        }
-        else
-        {
-            waitpid(my_pid, &status, 0);
-        }
+        cleanupAndExit(userInput, exit_status);
     }
-    cleanupAndExit(userInput, shellArgs);
-    return (0);
+
+    cleanupAndExit(userInput, exit_status);
 }
 
-void executeCommand(char *shellCommand, char **shellArgs, char *envp[])
-{
-    if (execve(shellCommand, shellArgs, envp) == -1)
-    {
-        perror("execve");
-        exit(EXIT_FAILURE);
-    }
-}
-
-void cleanupAndExit(char *userInput, char **shellArgs)
-{
+void cleanupAndExit(char *userInput, int exit_status) {
     free(userInput);
-    free(shellArgs);
-    exit(0);
+    exit(exit_status);
 }
 
-void removeLeadingSpaces(char *str)
-{
-    int len = strlen(str);
-    int i, j = 0;
-    int leadingSpace = 1;
+void executeCommand(char **shellArgs) {
+    pid_t pid = fork();
 
-    for (i = 0; i < len; i++)
-    {
-        if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i] != '\r')
-        {
-            leadingSpace = 0;
-        }
-
-        if (!leadingSpace)
-        {
-            str[j++] = str[i];
-        }
+    if (pid == -1) {
+        perror("fork");
+        return;
     }
 
-    str[j] = '\0';
-}
-
-void removeTrailingSpaces(char *str)
-{
-    int len = strlen(str);
-    int i = len - 1;
-
-    while (i >= 0 && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\r'))
-    {
-        str[i] = '\0';
-        i--;
+    if (pid == 0) {
+        if (execvp(shellArgs[0], shellArgs) == -1) {
+            perror("execvp");
+            exit(1);
+        }
+    } else {
+        int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            perror("waitpid");
+        }
     }
 }
 
-void printEnvironment()
-{
-    extern char **environ;
+ssize_t custom_getline(char **lineptr, size_t *n, FILE *stream) {
+    ssize_t bytesRead;
+    *lineptr = NULL;
+    *n = 0;
 
-    char **env;
-    for (env = environ; *env != NULL; env++)
-    {
-        printf("%s\n", *env);
-    }
-}
-
-char *findCommandPath(const char *command)
-{
-    char *path;
-    char *path_copy;
-    char *dir;
-    path = getenv("PATH");
-    path_copy = strdup(path);
-    dir = strtok(path_copy, ":");
-    if (path == NULL)
-    {
-        fprintf(stderr, "Error: PATH environment variable is not set.\n");
-        return NULL;
-    }
-
-    if (path_copy == NULL)
-    {
-        perror("strdup");
-        exit(EXIT_FAILURE);
-    }
-
-    while (dir != NULL)
-    {
-        char command_path[MAX_PATH_LEN];
-        snprintf(command_path, sizeof(command_path), "%s/%s", dir, command);
-        if (access(command_path, X_OK) == 0)
-        {
-            free(path_copy);
-            return strdup(command_path);
-        }
-
-        dir = strtok(NULL, ":");
-    }
-
-    free(path_copy);
-    return NULL;
-}
-
-void printShellArgs(char **shellArgs)
-{
-    int i = 0;
-    printf("start printing\n");
-    while (shellArgs[i] != NULL)
-    {
-        printf("shellArgs[%d]: %s\n", i, shellArgs[i]);
-        i++;
-    }
-    printf("end of printing\n");
-}
-
-ssize_t custom_getline(char **lineptr, size_t *n, FILE *stream)
-{
-    size_t buffer_size = 128;
-    ssize_t characters_read = 0;
-    int c;
-    char *temp;
-    char *line;
-
-    if (lineptr == NULL || n == NULL)
-    {
-        return -1;
-    }
-
-    if (*lineptr == NULL)
-    {
-        *lineptr = (char *)malloc(buffer_size);
-        if (*lineptr == NULL)
-        {
-            return -1;
-        }
-        *n = buffer_size;
-    }
-
-    line = *lineptr;
-
-    while (1)
-    {
-        c = fgetc(stream);
-
-        if (c == EOF)
-        {
-            if (characters_read == 0)
-            {
-                return -1;
-            }
-            break;
-        }
-
-        if (c == '\n')
-        {
-            line[characters_read] = '\0';
-            break;
-        }
-
-        line[characters_read] = (char)c;
-        characters_read++;
-
-        if ((unsigned long)characters_read >= *n)
-        {
-            buffer_size *= 2;
-            temp = realloc(line, buffer_size);
-            if (temp == NULL)
-            {
-                return -1;
-            }
-            line = temp;
-            *n = buffer_size;
-        }
-    }
-
-    *lineptr = line;
-    return characters_read;
+    bytesRead = getline(lineptr, n, stream);
+    return bytesRead;
 }
